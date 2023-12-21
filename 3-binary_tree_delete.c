7d9251b81971a8ae3559046c93845328e0840743
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-21 11:44:26 -0800

    made function to free the tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes a binary tree
+ * @tree: pointer to root of tree
+ **/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+
+	else
+		free(tree);
+}
