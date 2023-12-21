1b4e9c8516d1bebd8fe104faed80f89546843894
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-21 11:38:15 -0800

    betty did not like me copy pasting my own code lol

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,32 +10,32 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
-        binary_tree_t *temp = NULL;
-
-        new_node = malloc(sizeof(binary_tree_t));
-        if (new_node == NULL || parent == NULL)
-        {
-                free(new_node);
-                return (NULL);
-        }
-        new_node->n = value;
-        if (parent->right != NULL)
-        {
-                temp = parent->right;
-                parent->right = new_node;
-                parent->left = NULL;
-                new_node->parent = parent;
-                new_node->left = NULL;
-                new_node->right = temp;
-                temp->parent = new_node;
-        }
-        else
-        {
-                parent->right = new_node;
-                parent->left = NULL;
-                new_node->parent = parent;
-                new_node->right = NULL;
-                new_node->left = NULL;
-        }
-        return (new_node);
+	binary_tree_t *temp = NULL;
+	
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL || parent == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = value;
+	if (parent->right != NULL)
+	{
+		temp = parent->right;
+		parent->right = new_node;
+		parent->left = NULL;
+		new_node->parent = parent;
+		new_node->left = NULL;
+		new_node->right = temp;
+		temp->parent = new_node;
+	}
+	else
+	{
+		parent->right = new_node;
+		parent->left = NULL;
+		new_node->parent = parent;
+		new_node->right = NULL;
+		new_node->left = NULL;
+	}
+	return (new_node);
 }
