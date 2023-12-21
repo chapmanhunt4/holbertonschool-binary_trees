a820832143e45af630ecf9160933b87333ed62d9
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-21 11:27:53 -0800

    handled malloc, and fixed insertion position

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,17 +10,32 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
+	binary_tree_t *temp = NULL;
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL || parent == NULL)
+	{
+		free(new_node);
 		return (NULL);
-
+	}
 	new_node->n = value;
-	parent->left = new_node;
-	parent->right = NULL;
-	new_node->parent = parent;
-	new_node->right = NULL;
-	new_node->left = NULL;
-
+	if (parent->left != NULL)
+	{
+		temp = parent->left;
+		parent->left = new_node;
+		parent->right = NULL;
+		new_node->parent = parent;
+		new_node->right = NULL;
+		new_node->left = temp;
+		temp->parent = new_node;
+	}
+	else
+	{
+		parent->left = new_node;
+		parent->right = NULL;
+		new_node->parent = parent;
+		new_node->right = NULL;
+		new_node->left = NULL;
+	}
 	return (new_node);
 }
