2e5ed69cadacc9ace63c3c82024c9a9219851539
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-21 11:12:37 -0800

    fixed typo

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node->n = value;
-	parent->left = newnode;
+	parent->left = new_node;
 	parent->right = NULL;
 	new_node->parent = parent;
 	new_node->right = NULL;
