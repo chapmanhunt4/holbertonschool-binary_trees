b45381f38e53b040602dc2114c5876eaa3543d53
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-22 11:52:15 -0800

    added extra checks

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,10 +11,13 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree != NULL)
 	{
 		if (tree->left != NULL && tree->right != NULL)
-		{
+			return (1);
 
-		}
-		return (1);
+		if (tree->right == NULL && tree->left != NULL)
+			return (0);
+
+		if (tree->left == NULL && tree->right != NULL)
+			return (0);
 	}
 	return (0);
 }
