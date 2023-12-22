37a03825d6ed1618136c78444f7326a16977eb9c
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-22 07:38:17 -0800

    fixed typo

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,6 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		if (tree->right || tree->left)
 		{
 			return (binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left) + 1);
+		}
 	}
 	return (0);
 }
