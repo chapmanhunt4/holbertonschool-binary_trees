46085ef46c70ed520f68d0868365c5536ec2ad71
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-22 12:36:18 -0800

    added checks for first nodes

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,7 +14,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL || tree->right == NULL)
+	if (tree->left != NULL && tree->right == NULL)
+		return (0);
+
+	if (tree->right != NULL && tree->left == NULL)
 		return (0);
 
 	if (tree->left != NULL && tree->right != NULL)
