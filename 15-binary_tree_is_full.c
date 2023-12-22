599f504960038b75184a763ffad3fd7634221e5a
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-22 12:04:37 -0800

    made function call itself to iterate through rest of tree

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,16 +8,24 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree != NULL)
-	{
-		if (tree->left != NULL && tree->right != NULL)
-			return (1);
+	int right_count = 0;
+	int left_count = 0;
 
-		if (tree->right == NULL && tree->left != NULL)
-			return (0);
+	if (tree == NULL)
+		return (0);
 
-		if (tree->left == NULL && tree->right != NULL)
-			return (0);
-	}
-	return (0);
+	if (tree->left == NULL && tree->right != NULL)
+		return (0);
+
+	if (tree->right == NULL && tree->left != NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	left_count = binary_tree_is_full(tree->left);
+	right_count = binary_tree_is_full(tree->right);
+
+	if ((left_count == 0 && right_count == 1) || (left_count == 1 && right_count == 0))
+		return (0);
 }
