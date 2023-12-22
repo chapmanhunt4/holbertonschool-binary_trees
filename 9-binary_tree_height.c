235369afd692cafa6c72b4757299445d8758d0b2
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-22 06:36:25 -0800

    Betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,7 +6,7 @@
  * Return: the size, or 0 if null
  **/
 
- size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t right_h = 0;
 	size_t left_h = 0;
