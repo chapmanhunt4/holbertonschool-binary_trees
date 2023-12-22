f0a54304df544b39899de49982f05b3af28c1f09
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-22 11:46:19 -0800

    dont know why no work

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,8 +8,13 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	if (tree->left != NULL && tree->right != NULL)
+	if (tree != NULL)
+	{
+		if (tree->left != NULL && tree->right != NULL)
+		{
+
+		}
 		return (1);
+	}
+	return (0);
 }
